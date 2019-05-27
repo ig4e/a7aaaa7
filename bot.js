e229ac3528bedab48b113c4e71fea1d638891625
ig4e <50212311+ig4e@users.noreply.github.com>
2019-05-27 07:05:25 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
-const devs = ['464310531621388308'];
+const devs = ['389090790984515594'];
 const db = require('quick.db');
-const premium = ['464310531621388308']
+const premium = ['470896018603376640']
 const client = new Discord.Client();   
 const bot = new Discord.Client();   
 const giphy = require('giphy-api')();    
